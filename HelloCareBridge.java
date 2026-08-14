ca3e2650e89023569f136e714ca0201cf0f05671
Hemanth Bantwal <buntylearns@gmail.com>
2026-08-13 22:39:04 -0400

    First commit: HelloCareBridge

---
+++ HelloCareBridge.java
@@ -0,0 +1,6 @@
+public class HelloCareBridge {
+    public static void main(String[] args) {
+        System.out.println("Hello from CareBridge!");
+        System.out.println("Running on Java " + System.getProperty("java.version"));
+    }
+}
